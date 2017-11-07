55a5cc455ecf491b53d98c16e5077304f8e55e2e
Alexandre Boyer <alexandre.boyer@kedgebs.com>
2017-11-07 16:51:05 +0100

    Manque du front pour le style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tasks = Task.create!([{ name: 'CAF', description: 'Payer la caf'}, { name: 'Nettoyage', description: 'Aspirateur appart'}])
